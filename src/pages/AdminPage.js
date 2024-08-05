8b52862753610980836f841215b5f879919f40e2
Neeraj Reddy <neerajreddy528@gmail.com>
2024-08-05 09:13:56 +0530

    FInal Commit

---
+++ src/pages/AdminPage.js
@@ -0,0 +1,17 @@
+// src/pages/AdminPage.js
+import React from 'react';
+import LineChartComponent from '../components/LinearChartCOmponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const AdminPage = () => (
+  <div className="container mt-5">
+    <h1 className="mb-4">Sales and Orders Overview</h1>
+    <div className="card">
+      <div className="card-body">
+        <LineChartComponent />
+      </div>
+    </div>
+  </div>
+);
+
+export default AdminPage;
